44fb3fb1d25dbd1d3f1dca4ee7f0524990ad9eeb
jhonforero44 <jhonforero44@gmail.com>
2025-03-17 23:38:35 -0500

    Update Router

---
+++ 5-Clase/proyecto-rickandmorty-copalibertadores-miperfil/src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue') 
+    },
+    {
+      path: '/myprofile',
+      name: 'myprofile',
+      component: () => import('../views/MyProfile.vue') 
+    },
+    {
+      path: '/rickandmorty',
+      name: 'rickandmorty',
+      component: () => import('../views/RickAndMorty.vue') 
+    },
+    {
+      path: '/copalibertadores',
+      name: 'copalibertadores',
+      component: () => import('../views/CopaLibertadores.vue') 
+    }
+  ]
+});
+
+export default router;
+
